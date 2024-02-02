861225cc9a4d51a439247b84823327c98d5267e4
newnattapon395 <nattaponchumtim@gmail.com>
2024-02-02 16:28:52 +0700

    Front2

---
+++ Homepage/javasriptHome.js
@@ -0,0 +1,12 @@
+document.getElementById("searchForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // ป้องกันการส่งค่าแบบฟอร์ม
+
+    let searchTerm = document.getElementById("searchInput").value;
+
+    if (searchTerm.trim() == "Prang") {
+        var searchURL = "https://github.com/newnattapon395/First_Project/tree/main/index" + encodeURIComponent(searchTerm);
+        window.location.href = searchURL;
+    } else {
+        alert("กรอกใหม่อีกครั้ง");
+    }
+});
