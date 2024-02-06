3f84961a5441fffc64a32446fcd248b828a5fe50
newnattapon395 <nattaponchumtim@gmail.com>
2024-02-06 15:40:17 +0700

    navbar

--- testตัวอย่าง/javasript.js
+++ testตัวอย่าง/javasript.js
@@ -1,5 +1,14 @@
-window.addEventListener("scroll",function(){
-    var header = this.document.querySelector("header");
-    header.classList.toggle("sticky",window.scrollY > 50)
-})
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementById("head").style.padding = "20px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("head").style.backgroundColor = "rgb(66, 66, 66)" ;
+  } else {
+    document.getElementById("head").style.padding = "20px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("head").style.backgroundColor = "#000" ;
+
+  }
+}
